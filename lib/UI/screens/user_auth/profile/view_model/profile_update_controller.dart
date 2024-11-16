3defa74a9e48014b37ed71c5465fec780620d426
ikramulhasantanvir <ikramulhasantanvir@gmail.com>
2024-11-16 23:35:45 +0600

    fixed some issue

--- lib/UI/screens/user_auth/profile/view_model/profile_update_controller.dart
+++ lib/UI/screens/user_auth/profile/view_model/profile_update_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:task_manager_get_x/UI/screens/user_auth/profile/model/user_data_model.dart';
 import 'package:task_manager_get_x/data/data_controller/auth_controller.dart';
 import 'package:task_manager_get_x/data/models/network_response.dart';
@@ -7,24 +10,19 @@ import 'package:task_manager_get_x/data/services/network_caller.dart';
 import 'package:task_manager_get_x/data/utils/urls.dart';
 
 class ProfileUpdateController extends GetxController {
+  bool _inProgress = false;
+  String? _errorMessage;
+  String? _successMessage;
+  XFile? _selectedImage;
+
   bool get inProgress => _inProgress;
 
-  bool _inProgress = false;
-
-  String? _errorMessage;
-
-  String? _successMessage;
-
-  File? _selectedImage;
+  File? get selectedImage =>
+      _selectedImage != null ? File(_selectedImage!.path) : null;
 
   String? get errorMessage => _errorMessage;
 
   String? get successMessage => _successMessage;
-
-  void setImage(File image) {
-    _selectedImage = image;
-    update();
-  }
 
   Future<bool> updateProfile(
     String email,
@@ -43,20 +41,34 @@ class ProfileUpdateController extends GetxController {
       "lastName": lastName,
       "mobile": mobile,
     };
-    if (password != '') {
+
+    if (password.isNotEmpty) {
       requestBody['password'] = password;
     }
+
     if (_selectedImage != null) {
-      requestBody['photo'] = _selectedImage!.path;
+      File imageFile = File(_selectedImage!.path);
+      List<int>? compressedImageBytes =
+          await FlutterImageCompress.compressWithFile(
+        imageFile.path,
+        minWidth: 200, // Adjust as needed
+        minHeight: 200,
+        quality: 85,
+      );
+
+      if (compressedImageBytes != null) {
+        String base64Image = base64Encode(compressedImageBytes);
+        requestBody['photo'] = base64Image;
+      }
     }
+
     NetworkResponse networkResponse = await NetworkCaller.postRequest(
       url: Urls.profileUpdate,
       body: requestBody,
     );
+
     if (networkResponse.isSuccess) {
-      final UserData userData = UserData.fromJson(
-        requestBody,
-      );
+      final UserData userData = UserData.fromJson(requestBody);
       await AuthController.saveUserData(userData);
       _successMessage = 'Profile updated';
       Get.back();
@@ -64,8 +76,19 @@ class ProfileUpdateController extends GetxController {
     } else {
       _errorMessage = networkResponse.errorMassage;
     }
+
     _inProgress = false;
     update();
     return isSuccess;
+  }
+
+  Future<void> pickImage() async {
+    ImagePicker imagePicker = ImagePicker();
+    XFile? pickedImage =
+        await imagePicker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      _selectedImage = pickedImage;
+    }
+    update();
   }
 }
--- lib/UI/screens/user_auth/profile/widgets/profile_image_container.dart
+++ lib/UI/screens/user_auth/profile/widgets/profile_image_container.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:task_manager_get_x/UI/screens/user_auth/profile/view_model/profile_update_controller.dart';
 import 'package:task_manager_get_x/data/data_controller/auth_controller.dart';
 
@@ -13,51 +11,40 @@ class ProfileImageContainer extends StatefulWidget {
 }
 
 class _ProfileImageContainerState extends State<ProfileImageContainer> {
-  File? _selectedImage;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: _pickImage,
-      child: Container(
-        padding: const EdgeInsets.all(2),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.grey[600],
+    return GetBuilder<ProfileUpdateController>(builder: (controller) {
+      return InkWell(
+        onTap: controller.pickImage,
+        child: Container(
+          padding: const EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.grey[600],
+          ),
+          child: CircleAvatar(
+            backgroundColor: Colors.grey[200],
+            maxRadius: 54,
+            backgroundImage: controller.selectedImage != null
+                ? FileImage(controller.selectedImage!)
+                : (AuthController.userData?.photo != null &&
+                        AuthController.userData!.photo!.isNotEmpty)
+                    ? NetworkImage(AuthController.userData!.photo!)
+                        as ImageProvider
+                    : null,
+            child: (controller.selectedImage == null &&
+                    (AuthController.userData?.photo == null ||
+                        AuthController.userData!.photo!.isEmpty))
+                ? Icon(
+                    Icons.person,
+                    size: 98,
+                    color: Colors.grey[400],
+                  )
+                : null,
+          ),
         ),
-        child: CircleAvatar(
-          backgroundColor: Colors.grey[200],
-          maxRadius: 54,
-          backgroundImage: _selectedImage != null
-              ? FileImage(_selectedImage!)
-              : (AuthController.userData?.photo != null &&
-              AuthController.userData!.photo!.isNotEmpty)
-              ? NetworkImage(AuthController.userData!.photo!) as ImageProvider
-              : null,
-          child: (_selectedImage == null &&
-              (AuthController.userData?.photo == null ||
-                  AuthController.userData!.photo!.isEmpty))
-              ? Icon(
-            Icons.person,
-            size: 98,
-            color: Colors.grey[400],
-          )
-              : null,
-
-        ),
-      ),
-    );
-  }
-
-  Future<void> _pickImage() async {
-    final ImagePicker imagePicker = ImagePicker();
-    final XFile? pickImage =
-        await imagePicker.pickImage(source: ImageSource.gallery);
-    if (pickImage != null) {
-      _selectedImage = File(pickImage.path);
-      setState(() {});
-
-      Get.find<ProfileUpdateController>().setImage(_selectedImage!);
-    }
+      );
+    });
   }
 }
